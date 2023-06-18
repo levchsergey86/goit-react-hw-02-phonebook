c722cd34497f05376a0d9fb0bf7da1018f5e7543
levchsergey86 <levchsergey86@gmail.com>
2023-06-18 16:25:40 +0300

    Fix pattern filterContac

--- src/components/FilterContacts/FilterContacts.jsx
+++ src/components/FilterContacts/FilterContacts.jsx
@@ -8,7 +8,7 @@ const FilterContacts = ({ handleFilterChange }) => {
       <input
         type="text"
         name="filter"
-        pattern="[a-zA-Zа-яА-Я\s'-]"
+        pattern="^[A-Za-z\u0080-\uFFFF ']+$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         required
         onChange={handleFilterChange}
